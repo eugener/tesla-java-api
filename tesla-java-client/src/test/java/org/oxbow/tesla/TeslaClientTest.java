7260ab99494a49102a61c22f2f9805e297d58166
Eugene Ryzhikov <eryzhikov@gmail.com>
2018-10-24 18:34:48 -0400

    Refactor tests

--- tesla-java-client/src/test/java/org/oxbow/tesla/TeslaClientTest.java
+++ tesla-java-client/src/test/java/org/oxbow/tesla/TeslaClientTest.java
@@ -1,58 +1,47 @@
 package org.oxbow.tesla;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.oxbow.tesla.domain.ChargeState;
 import org.oxbow.tesla.domain.Vehicle;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
 
-public class TeslaClientTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeslaClientTest {
 
     private String user = System.getProperty("tesla.user");
     private String pswd = System.getProperty("tesla.pswd");
 
     private TeslaClient client = TeslaClient.Builder(user, pswd).build();
 
-    private List<Vehicle> vehicles = null;
-    private Long vehicleId = null;
+    private Lazy<List<Vehicle>> vehicles = new Lazy<>(() -> client.getVehicles());
+    private Lazy<Long>         vehicleId = new Lazy<>(() -> vehicles.get().get(0).getId());
 
-
-    private List<Vehicle> getVehicles() {
-        if ( vehicles == null ) {
-            vehicles = client.getVehicles();
-        }
-        return vehicles;
-    }
-
-    private Long getVehicleId() {
-        if ( vehicleId == null) {
-            Long vehicleId = getVehicles().get(0).getId();
-            System.out.println(vehicleId);
-        }
-        return vehicleId;
+    @Test
+    @DisplayName("Wake Command")
+    void test1() {
+        assertTrue( client.wake( vehicleId.get() ));
     }
 
     @Test
-    public void wakeCommand() {
-        assertTrue( client.wake(getVehicleId()) );
-    }
-
-
-        @Test
-    public void vehicleAPI() {
-        assertTrue( getVehicles().size() > 0);
-        Long id = getVehicles().get(0).getId();
+    @DisplayName("Vehicle API")
+    void test2() {
+        assertFalse( vehicles.get().isEmpty());
+        Long id = vehicleId.get();
+        System.out.println(id);
         Vehicle vehicle = client.getVehicle(id);
         assertEquals( id, vehicle.getId());
     }
 
     @Test
-    public void chargeStateAPI() {
-        client.wake(getVehicleId());
-        ChargeState chargeState = client.getChargeState(getVehicleId());
-        assertTrue( chargeState != null);
+    @DisplayName("Charge State API")
+    void test3() {
+        client.wake(vehicleId.get());
+        ChargeState chargeState = client.getChargeState(vehicleId.get());
+        assertNotNull(chargeState);
     }
 
 }
+
